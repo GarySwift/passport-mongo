76ac4099ffd1f79ef963052cf2734c2f38afd782
Gary Swift <garyswiftmail@gmail.com>
2015-04-09 19:10:35 +0100

    first commit

---
+++ models/user.js
@@ -0,0 +1,20 @@
+// var mongoose = require('mongoose');
+ 
+// module.exports = mongoose.model('User',{
+//         username: String,
+//     password: String,
+//     email: String,
+//     gender: String,
+//     address: String
+// });
+
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('User',{
+	id: String,
+	username: String,
+	password: String,
+	email: String,
+	firstName: String,
+	lastName: String
+});
